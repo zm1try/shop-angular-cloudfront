c0ec6fcceef5e86a791f0832cdbdd497b717cd76
Dzmitry_Yarash <dzmitry_yarash@epam.com>
2023-04-16 22:09:07 +0300

    feat: connected import api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,14 +5,14 @@ export const environment: Config = {
   apiEndpoints: {
     product: 'https://k4bw7kw927.execute-api.us-east-1.amazonaws.com/dev',
     order: 'https://.execute-api.us-east-1.amazonaws.com/dev',
-    import: 'https://.execute-api.us-east-1.amazonaws.com/dev',
+    import: 'https://rwzs06n2za.execute-api.us-east-1.amazonaws.com/dev',
     bff: 'https://k4bw7kw927.execute-api.us-east-1.amazonaws.com/dev',
     cart: 'https://.execute-api.us-east-1.amazonaws.com/dev',
   },
   apiEndpointsEnabled: {
     product: true,
     order: false,
-    import: false,
+    import: true,
     bff: true,
     cart: false,
   },
